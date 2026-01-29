abb94c8ab4cf6b21a045697340b05238580f2b20
MINJAE KIM <rkfsky920@gmail.com>
2026-01-29 21:43:08 +0900

    [Silver II] Title: 베르트랑 공준, Time: 144 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -33,7 +33,7 @@ public class Main {
         if (num == 2 || num == 3) return true;
         if (num % 2 == 0 || num % 3 == 0) return false;
         
-        for (int i = 5; i <= Math.sqrt(num); i++) {
+        for (int i = 5; i <= Math.sqrt(num); i += 6) {
             if (num % i == 0 || num % (i + 2) == 0) return false;
         }
         return true;
