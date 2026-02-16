0c0a1940d4347caedba723c7e7aa49c5f1d43265
Minjae Kim <rkfsky920@gmail.com>
2026-02-17 00:44:35 +0900

    [Gold IV] Title: 문자열 폭발, Time: 1044 ms, Memory: 35348 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String bomb = br.readLine();
+        
+        int strLen = str.length();
+        int bombLen = bomb.length();
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < strLen; i++) {
+            char ch = str.charAt(i);
+            stack.push(ch);
+            
+            boolean deleted = true;
+            if (ch == stack.peek() && stack.size() >= bombLen) {
+                for (int j = 0; j < bombLen; j++) {
+                    int deleteLen = stack.size() - bombLen + j;
+                    if (stack.elementAt(deleteLen) != bomb.charAt(j)) deleted = false;
+                }
+            }
+            
+            if (deleted && stack.size() >= bombLen) {
+                for (int j = 0; j < bombLen; j++) {
+                    stack.pop();
+                }
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (char ch : stack) {
+            sb.append(ch);
+        }
+        
+        if (sb.toString().isEmpty()) {
+            sb.append("FRULA");
+        }
+        
+        System.out.println(sb);
+    }
+}
