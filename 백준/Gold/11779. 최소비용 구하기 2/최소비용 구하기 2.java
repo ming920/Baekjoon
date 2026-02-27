f8889b7a32dd2c1569d425b20a180b5feee0fd63
Minjae Kim <rkfsky920@gmail.com>
2026-02-27 16:23:50 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 504 ms, Memory: 52172 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static class Node implements Comparable<Node> {
+        int to;
+        int cost;
+        
+        public Node(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+        
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.cost, other.cost);
+        }
+    }
+    
+    public static List<Node>[] graph;
+    public static int[] dist;
+    public static int[] route;
+    public static Stack<Integer> stack = new Stack<>();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st;
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            
+            graph[a].add(new Node(b, c));
+        }
+        
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+        
+        dist = new int[N + 1];
+        Arrays.fill(dist, 1_000_000_000);
+        dist[start] = 0;
+        
+        route = new int[N + 1];
+        dijkstra(start);
+        
+        System.out.println(dist[end]);
+        printRoute(end);
+        stack.push(start);
+        System.out.println(stack.size());
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+    
+    public static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+        
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+            
+            if (now.cost > dist[now.to]) continue;
+            
+            for (Node next : graph[now.to]) {
+                if (dist[next.to] > dist[now.to] + next.cost) {
+                    dist[next.to] = dist[now.to] + next.cost;
+                    pq.add(new Node(next.to, dist[next.to]));
+                    route[next.to] = now.to;
+                }
+            }
+        }
+    }
+    
+    public static void printRoute(int index) {
+        if (route[index] != 0) {
+            stack.push(index);
+            printRoute(route[index]);
+        }
+    }
+}
