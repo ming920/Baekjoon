11388a2dbab42e1d2aa293a5a73d8b7415c20019
Minjae Kim <rkfsky920@gmail.com>
2026-04-02 16:14:55 +0900

    [Gold III] Title: 소수의 연속합, Time: 216 ms, Memory: 27236 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -4,50 +4,42 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         
-        int num = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
-        for (int i = 2; i <= 4_000_000; i++) {
-            if (isPrime(i)) {
-                list.add(i);
+        // 1. 에라토스테네스의 체
+        boolean[] isPrime = new boolean[N + 1];
+        Arrays.fill(isPrime, true);
+        if (N >= 0) isPrime[0] = false;
+        if (N >= 1) isPrime[1] = false;
+        
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
         
-        long[] A = new long[list.size() + 1];
-        for (int i = 1; i <= list.size(); i++) {
-            A[i] = list.get(i - 1);
+        // 2. 소수 리스트 생성
+        List<Integer> primes = new ArrayList<>();
+        for (int i = 2; i <= N; i++) {
+            if (isPrime[i]) primes.add(i);
         }
         
-        for (int i = 1; i <= list.size(); i++) {
-            A[i] += A[i - 1];
-        }
+        int left = 0, right = 0;
+        int sum = 0, result = 0;
         
-        int left = 0, right = 0, result = 0;
-        
-        while (right < A.length) {
-            long diff = A[right] - A[left];
+        while (true) {
+            if (sum >= N) {
+                sum -= primes.get(left++);
+            } else {
+                if (right == primes.size()) break;
+                sum += primes.get(right++);
+            }
             
-            if (diff < num) right++;
-            else if (diff > num) left++;
-            else {
-                result++;
-                right++;
-                left++;
-            }
+            if (sum == N) result++;
         }
-        
+
         System.out.println(result);
-    }
-    
-    public static boolean isPrime(int num) {
-        if (num <= 1) return false;
-        if (num == 2 || num == 3) return true;
-        if (num % 2 == 0 || num % 3 == 0) return false;
-        
-        for (int i = 5; i * i <= num; i++) {
-            if (num % i == 0 || num % (i + 2) == 0) return false;
-        }
-        
-        return true;
     }
 }
