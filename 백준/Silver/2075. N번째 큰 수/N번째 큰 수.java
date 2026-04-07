7aa50db9bb8804969e37302a6df892fbe798b472
Minjae Kim <rkfsky920@gmail.com>
2026-04-07 15:44:18 +0900

    [Silver III] Title: N번째 큰 수, Time: 760 ms, Memory: 273768 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -6,37 +6,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         
-        int[][] arr = new int[N][N];
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+           return Integer.compare(b, a); 
+        });
+        
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                pq.add(Integer.parseInt(st.nextToken()));
             }
-        }
-        
-        int[] size = new int[N];
-        for (int i = 0; i < N; i++) {
-            size[i] = N - 1;
         }
         
         int cnt = 0;
-        int answer = Integer.MAX_VALUE;
-        while (cnt < N) {
-            int index = 0;
-            int num = Integer.MIN_VALUE;
-            
-            for (int i = 0; i < N; i++) {
-                if (arr[size[i]][i] > num) {
-                    num = arr[size[i]][i];
-                    index = i;
-                }
-            }
-            
-            answer = Math.min(answer, num);
-            size[index]--;
+        while (cnt < N - 1) {
+            pq.poll();
             cnt++;
         }
         
-        System.out.println(answer);
+        System.out.println(pq.poll());
     }
 }
