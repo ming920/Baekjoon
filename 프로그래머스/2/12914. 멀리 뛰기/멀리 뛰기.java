9a00ceccb952cd7a6cc51805eb0b1db38d605dd6
Minjae Kim <rkfsky920@gmail.com>
2026-05-11 14:06:58 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.09 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long solution(int n) {
+        if (n < 3) {
+            return n;
+        }
+        
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+}
